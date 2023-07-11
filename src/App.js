a6edbd738a88222380d1c6cdc7aa52a19a57797c
DavitGyogchyan <mikayel1999@mail.com>
2023-07-11 23:06:24 +0000

    second commit, useReducer usage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import {useState} from "react"
+import {useState, useReducer} from "react";
 import './App.css';
-import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
 import TodoFooter from "./TodoFooter";
+
+function reducer(state, action) {
+  if(action.type === "add") {
+    return [
+      ...state,
+      {
+        id: Math.random(),
+        text: action.payload.text,
+        isCompleted: false
+      }
+    ];
+  } else if(action.type === "delete") {
+    return state.filter((t) => t.id !== action.payload.id);
+  } else if(action.type === "clear-completed") {
+    return state.filter((todo) => !todo.isCompleted);
+  } else if(action.type === "update") {
+    return state.map((todo) => {
+      if(todo.id === action.payload.updatedTodo.id) {
+        return action.payload.updatedTodo;
+      }
+      return todo;
+    });
+  }
+}
 
 function App() {
 
-  const [todos, setTodos] = useState([
+  const [todos, dispatch] = useReducer(reducer, [
     {
       id: Math.random(),
       text: "Learn JS",
@@ -23,41 +47,47 @@ function App() {
       isCompleted: false
     }
   ]);
+  
 
   return (
     <div className="App">
       <header>
-        <h1 className="todoAppTitle">todos</h1>
-      </header>
-
-      <TodoForm onAdd = {(text) => {
-        setTodos([
-          ...todos,
-          {
-            id: Math.random(),
-            text: text,
-            isCompleted: false
+          <h1 className="todoAppTitle">todos</h1>
+        </header>
+        
+      <TodoForm onAdd={(text) => {
+        dispatch({
+          type: 'add',
+          payload: {
+            text: text
           }
-        ])
+        });
       }}/>
       <TodoList 
-        todos={todos}
+        todos={todos} 
         onDelete={(todo) => {
-          setTodos(todos.filter((t) => t.id !== todo.id))
+          dispatch({
+            type: "delete",
+            payload: {
+              id: todo.id
+            }
+          });
         }}
         onChange={(newTodo) => {
-          setTodos(todos.map((todo) => {
-            if(todo.id === newTodo.id) {
-              return newTodo
+          dispatch({
+            type: "update",
+            payload: {
+              updatedTodo: newTodo
             }
-            return todo
-          }))
+          });
         }}
       />
-      <TodoFooter todos = {todos} onClearCompleted={() => {
-        setTodos(todos.filter((todo) => !todo.isCompleted))
-      }}/> 
-    </div> 
+      <TodoFooter todos={todos} onClearCompleted={() => {
+        dispatch({
+          type: "clear-completed"
+        });
+      }}/>
+    </div>
   );
 }
 
